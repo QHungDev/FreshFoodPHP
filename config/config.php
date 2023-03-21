e6d16db4dbf3f12c85c80febbb3cfcaa5cfe9aa5
Quốc Hưng <truongquochung204@gmail.com>
2023-03-21 16:05:32 +0700

    Add project files.

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "12345678");
+define("DB_NAME", "freshfoodstore");
+$serveName ="localhost";
+$username ="root";
+$password ="12345678";
+$databaseName ="freshfoodstore";
+$conn = mysqli_connect($serveName,$username,$password,$databaseName);
+?>
